c83f16821e441f02f98b273891c85ac7924e3072
SkyLord348 <70288178+SkyLord348@users.noreply.github.com>
2020-12-01 13:25:28 -0600

    finished commit

---
+++ CS_ASP_016LPB/MainWindow.xaml.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Windows;
+using System.Windows.Controls;
+
+
+namespace CS_ASP_016LPB
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        // Get First Date
+
+        private void myCalendar_SelectedDatesChanged(object sender, SelectionChangedEventArgs e)
+        {
+
+            resultLabel.Content = myCalendar.SelectedDate.ToString();
+
+
+
+
+        }
+
+
+
+
+
+    }
+}
